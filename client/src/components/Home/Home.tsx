16b528065ea35eca56f575fb58aba77753a7ae22
Becca Rothwell <becca.rothwell@gmail.com>
2023-12-23 17:19:06 +0000

    feat: Add basic MainLayout and Home styles

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,3 +1,15 @@
-const Home: React.FC = () => <>Home!</>;
+import "./Home.scss";
+
+const Home: React.FC = () => (
+  <>
+    <h1 className="page__title">
+      Welcome to the home of the Justice Department of Fakelandia.
+    </h1>
+    <p className="page__text">
+      Here you can browse a list of recent misdemeanours committed by our
+      citizens, or you can confess to your own misdemeanour.
+    </p>
+  </>
+);
 
 export default Home;

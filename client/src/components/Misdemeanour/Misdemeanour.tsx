08dcbded701b31b8f8ef025d84f62898f56ca799
Becca Rothwell <5334385+beccarothwell@users.noreply.github.com>
2023-12-26 18:26:49 +0000

    feat:Implemet useFetch in App and createContext for misdemeanours, useContext for MisdemeanoursContext in Misdemeanour page component

--- client/src/components/Misdemeanour/Misdemeanour.tsx
+++ client/src/components/Misdemeanour/Misdemeanour.tsx
@@ -1,3 +1,18 @@
-const Misdemeanour: React.FC = () => <>Misdemeanour!</>;
+import { useContext } from "react";
+import { MisdemeanoursContext } from "../../App";
+
+const Misdemeanour: React.FC = () => {
+  const { data, isFetching, errorMessage } = useContext(MisdemeanoursContext);
+
+  const misdemeanours = data ? data.misdemeanours : [];
+
+  return (
+    <>
+      {isFetching && "...Loading"}
+      {misdemeanours && misdemeanours[0]?.citizenId}
+      {errorMessage && errorMessage}
+    </>
+  );
+};
 
 export default Misdemeanour;

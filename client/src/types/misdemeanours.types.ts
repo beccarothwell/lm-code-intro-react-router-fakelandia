f2ee369c7782a04d90bc435efa1b8b2ddca93316
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-01 16:46:05 +0000

    feat: Add MISDEMEANOURS_DISPLAY_TEXT to misdemeanours types and Map to MISDEMEANOURS_KINDS, implement MisdemeanoursTable in MisdemeanourPage, useMemo to fetch data to add display text and filters

--- client/src/types/misdemeanours.types.ts
+++ client/src/types/misdemeanours.types.ts
@@ -1,16 +1,33 @@
 export const MISDEMEANOURS = [
-	'rudeness',
-	'vegetables',
-	'lift',
-	'united',
+  "rudeness",
+  "vegetables",
+  "lift",
+  "united",
 ] as const;
 export type MisdemeanourKind = (typeof MISDEMEANOURS)[number];
 
-export const JUST_TALK = 'just-talk';
+export const MISDEMEANOURS_DISPLAY_TEXT = [
+  "Mild Public Rudeness 🤪",
+  "Not Eating Your Vegetables 🥗",
+  "Speaking in a Lift 🗣",
+  "Supporting Manchester United 😈",
+] as const;
+export type MisdemeanourKindDisplayText =
+  (typeof MISDEMEANOURS_DISPLAY_TEXT)[number];
+
+export const MISDEMEANOURS_MAP = new Map<
+  MisdemeanourKind,
+  MisdemeanourKindDisplayText
+>();
+MISDEMEANOURS.forEach((key, i) =>
+  MISDEMEANOURS_MAP.set(key, MISDEMEANOURS_DISPLAY_TEXT[i])
+);
+
+export const JUST_TALK = "just-talk";
 export type JustTalk = typeof JUST_TALK;
 
 export type Misdemeanour = {
-	citizenId: number;
-	misdemeanour: MisdemeanourKind;
-	date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
+  citizenId: number;
+  misdemeanour: MisdemeanourKind;
+  date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
 };

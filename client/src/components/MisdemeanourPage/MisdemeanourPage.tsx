37a241f2d3d44aecf2471ba2e9d445a4e7c65767
Becca Rothwell <5334385+beccarothwell@users.noreply.github.com>
2024-01-01 16:46:05 +0000

    feat: Add MISDEMEANOURS_DISPLAY_TEXT to misdemeanours types and Map to MISDEMEANOURS_KINDS, implement MisdemeanoursTable in MisdemeanourPage, useMemo to fetch data to add display text and filters

--- client/src/components/MisdemeanourPage/MisdemeanourPage.tsx
+++ client/src/components/MisdemeanourPage/MisdemeanourPage.tsx
@@ -1,15 +1,59 @@
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { MisdemeanoursContext } from "../../App";
+import MisdemeanoursTable from "../MisdemeanoursTable/MisdemeanoursTable";
+import { MISDEMEANOURS_MAP } from "../../types/misdemeanours.types";
 
 const MisdemeanourPage: React.FC = () => {
   const { data, isFetching, errorMessage } = useContext(MisdemeanoursContext);
 
-  const misdemeanours = data ? data.misdemeanours : [];
+  const misdemeanourValues = useMemo(
+    () =>
+      data?.misdemeanours.map((misdemeanourObj) => {
+        const { misdemeanour } = misdemeanourObj;
+
+        return {
+          ...misdemeanourObj,
+          misdemeanour: {
+            text: MISDEMEANOURS_MAP.get(misdemeanour) ?? misdemeanour,
+            value: misdemeanour,
+          },
+        };
+      }),
+    [data]
+  );
+
+  const misdemeanourFilters = useMemo(
+    () => [
+      ...new Map(
+        misdemeanourValues?.map(({ misdemeanour }) => [
+          misdemeanour.value,
+          { ...misdemeanour, text: misdemeanour.text.slice(0, -2) },
+        ])
+      ).values(),
+    ],
+    [misdemeanourValues]
+  );
+
+  const misdemeanourColumnHeads = [
+    { key: "citizenId", title: "Citizen ID" },
+    { key: "date", title: "Date" },
+    {
+      key: "misdemeanour",
+      title: "Misdemeanour",
+      filters: misdemeanourFilters,
+    },
+    { key: "punishmentIdea", title: "Punishment Idea" },
+  ];
 
   return (
     <>
       {isFetching && "...Loading"}
-      {misdemeanours && misdemeanours[0]?.citizenId}
+      {!isFetching && misdemeanourValues && misdemeanourFilters && (
+        <MisdemeanoursTable
+          bodyData={misdemeanourValues}
+          headerData={misdemeanourColumnHeads}
+        />
+      )}
       {errorMessage && errorMessage}
     </>
   );

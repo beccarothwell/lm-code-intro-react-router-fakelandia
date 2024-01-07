c1a03aa23738ed4396a117c73d6b3319d8b96723
Becca Rothwell <5334385+beccarothwell@users.noreply.github.com>
2024-01-07 21:55:48 +0000

    feat: Rename MISDEMEANOUR_DISPLAY_TEXT and move Misdemeanour emojis into seperate const array and type, create new Map MISDEMEANOUS_TEXT_WITH_EMOJI_MAP and new template literal combination type MisdemeanoutKindTextEmoji, add JUST_TALK_TEXT as const and JustTalkText type

--- client/src/types/misdemeanours.types.ts
+++ client/src/types/misdemeanours.types.ts
@@ -6,25 +6,44 @@ export const MISDEMEANOURS = [
 ] as const;
 export type MisdemeanourKind = (typeof MISDEMEANOURS)[number];
 
-export const MISDEMEANOURS_DISPLAY_TEXT = [
-  "Mild Public Rudeness 🤪",
-  "Not Eating Your Vegetables 🥗",
-  "Speaking in a Lift 🗣",
-  "Supporting Manchester United 😈",
+export const MISDEMEANOURS_TEXT = [
+  "Mild Public Rudeness",
+  "Not Eating Your Vegetables",
+  "Speaking in a Lift",
+  "Supporting Manchester United",
 ] as const;
-export type MisdemeanourKindDisplayText =
-  (typeof MISDEMEANOURS_DISPLAY_TEXT)[number];
+export type MisdemeanourKindText = (typeof MISDEMEANOURS_TEXT)[number];
+
+export const MISDEMEANOURS_EMOJIS = ["🤪", "🥗", "🗣", "😈"] as const;
+export type MisdemeanourKindEmoji = (typeof MISDEMEANOURS_EMOJIS)[number];
+
+export type MisdemeanourKindTextEmoji =
+  `${MisdemeanourKindText} ${MisdemeanourKindEmoji}`;
 
 export const MISDEMEANOURS_MAP = new Map<
   MisdemeanourKind,
-  MisdemeanourKindDisplayText
+  MisdemeanourKindText
 >();
 MISDEMEANOURS.forEach((key, i) =>
-  MISDEMEANOURS_MAP.set(key, MISDEMEANOURS_DISPLAY_TEXT[i])
+  MISDEMEANOURS_MAP.set(key, MISDEMEANOURS_TEXT[i])
+);
+
+export const MISDEMEANOURS_TEXT_WITH_EMOJI_MAP = new Map<
+  MisdemeanourKind,
+  MisdemeanourKindTextEmoji
+>();
+MISDEMEANOURS.forEach((key, i) =>
+  MISDEMEANOURS_TEXT_WITH_EMOJI_MAP.set(
+    key,
+    `${MISDEMEANOURS_TEXT[i]} ${MISDEMEANOURS_EMOJIS[i]}`
+  )
 );
 
 export const JUST_TALK = "just-talk";
 export type JustTalk = typeof JUST_TALK;
+
+export const JUST_TALK_TEXT = "Just Talk";
+export type JustTalkText = typeof JUST_TALK_TEXT;
 
 export type Misdemeanour = {
   citizenId: number;

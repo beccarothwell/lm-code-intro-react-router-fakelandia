900709dcb7893bfebb1d75d2d235fcda715f9f07
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-07 13:14:44 +0000

    feat: Add text to ConfessionPage

--- client/src/components/ConfessionPage/ConfessionPage.tsx
+++ client/src/components/ConfessionPage/ConfessionPage.tsx
@@ -1,3 +1,16 @@
-const ConfessionPage: React.FC = () => <>Confession!</>;
+const ConfessionPage: React.FC = () => {
+  return (
+    <>
+      <p className="page__text">
+        It's very difficult to catch people committing misdemeanours so we
+        appreciate it when citizens confess to us directly.
+      </p>
+      <p className="page__text">
+        However, if you're just having a hard day and need to vent then you're
+        welcome to contact us here too. Up to you!
+      </p>
+    </>
+  );
+};
 
 export default ConfessionPage;

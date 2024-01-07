c7adb0667010fd708ed581fcc901f0e659c5053e
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-07 13:00:14 +0000

    refactor: Remove unnecessary renderWithRouter from HomePage and NotFound tests

--- client/src/components/HomePage/HomePage.test.tsx
+++ client/src/components/HomePage/HomePage.test.tsx
@@ -1,9 +1,8 @@
-import { screen } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import HomePage from "./HomePage";
-import { renderWithRouter } from "../../test_helpers/render_with_router";
 
 test("Given the required props, When the component renders, Then a level 1 heading with the class page__title should be present", () => {
-  renderWithRouter(<HomePage />);
+  render(<HomePage />);
 
   const someHeading = screen.getByRole("heading");
 
@@ -13,7 +12,7 @@ test("Given the required props, When the component renders, Then a level 1 headi
 });
 
 test("Given the required props, When the component renders, Then the welcome text should be present", () => {
-  renderWithRouter(<HomePage />);
+  render(<HomePage />);
 
   const someHeadingText = screen.getByText(
     "Welcome to the home of the Justice Department of Fakelandia."
@@ -23,7 +22,7 @@ test("Given the required props, When the component renders, Then the welcome tex
 });
 
 test("Given the required props, When the component renders, Then a p tag with the appropriate text and class page__text should be present", () => {
-  renderWithRouter(<HomePage />);
+  render(<HomePage />);
 
   const someParagraphText = screen.getByText(
     "Here you can browse a list of recent misdemeanours committed by our citizens, or you can confess to your own misdemeanour."
--- client/src/components/NotFound/NotFound.test.tsx
+++ client/src/components/NotFound/NotFound.test.tsx
@@ -1,9 +1,8 @@
-import { screen } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import NotFound from "./NotFound";
-import { renderWithRouter } from "../../test_helpers/render_with_router";
 
 test("Given the required props, When the component renders, Then the text should be present", () => {
-  renderWithRouter(<NotFound />);
+  render(<NotFound />);
 
   const someText = screen.getByText("Not Found!");
 

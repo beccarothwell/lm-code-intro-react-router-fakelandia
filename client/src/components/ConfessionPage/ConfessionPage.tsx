4e2550f4c1b8d905e2ed0ea5f10cf02b0ac9357c
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-08 10:38:59 +0000

    feat: Attempt at updating the use_fetch hook to handle POST requests. Committed for review and commented out as it currently does not work as intended

--- client/src/components/ConfessionPage/ConfessionPage.tsx
+++ client/src/components/ConfessionPage/ConfessionPage.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ConfessionForm from "../ConfessionForm/ConfessionForm";
 import { ConfessionFormData } from "../ConfessionForm/ConfessionForm.types";
+import { useFetch } from "../../hooks/use_fetch/use_fetch";
 
 const DEFAULT_INPUT_DATA: ConfessionFormData = {
   subject: "",
@@ -15,7 +16,55 @@ const ConfessionPage: React.FC = () => {
   const submitData = (data: ConfessionFormData) => {
     setSubmissionData(data);
   };
-  console.log(submissionData);
+
+  /*const submission = useFetch(
+    "http://localhost:8080/api/confess/",
+    "POST",
+    submissionData
+  );
+  console.log(submission);*/
+
+  /*useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/api/confess/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(submissionData),
+          //signal: abortController.signal,
+        });
+        //setIsFetching(false);
+        if (!response.ok) {
+          if (response.status === 404) {
+            //setErrorMessage(`${response.status} Not Found`);
+          } else if (response.status === 418) {
+            //setErrorMessage(`${response.status} I'm a tea pot, silly`);
+          } else if (response.status === 500) {
+            //setErrorMessage(
+            //   `${response.status} Oops... something went wrong, try again 🤕`
+            // );
+          } else {
+            //setErrorMessage(`${response.status} ${response.statusText}`);
+          }
+        }
+        if (response.status === 200 || response.status === 201) {
+          const json = await response.json();
+          //setData(json);
+          console.log(json);
+        }
+      } catch (e: unknown) {
+        //setIsFetching(false);
+        console
+          .log
+          //  isError(e) && e.name !== "AbortError" ? e.message : "Unknown error!"
+          ();
+      }
+    };
+    fetchData();
+  }, [submissionData]);*/
+
   return (
     <>
       <p className="page__text">

2411cc4820cf40f30c2836dd1cc906ebd885ccec
Becca Rothwell <becca.rothwell@gmail.com>
2024-01-08 00:51:55 +0000

    feat: Add ConfessionForm component to ConfessionPage

--- client/src/components/ConfessionPage/ConfessionPage.tsx
+++ client/src/components/ConfessionPage/ConfessionPage.tsx
@@ -1,4 +1,10 @@
+//import { useState } from "react";
+import ConfessionForm from "../ConfessionForm/ConfessionForm";
+
 const ConfessionPage: React.FC = () => {
+  /*const [inputData, setInputData] =
+    useState<ConfessionFormData>(DEFAULT_INPUT_DATA);*/
+
   return (
     <>
       <p className="page__text">
@@ -9,6 +15,7 @@ const ConfessionPage: React.FC = () => {
         However, if you're just having a hard day and need to vent then you're
         welcome to contact us here too. Up to you!
       </p>
+      <ConfessionForm />
     </>
   );
 };

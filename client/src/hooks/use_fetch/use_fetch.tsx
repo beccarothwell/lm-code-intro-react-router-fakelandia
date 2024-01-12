15584dfe7cb4259403902339938721920cdb422b
Becca Rothwell <5334385+beccarothwell@users.noreply.github.com>
2024-01-12 11:40:17 +0000

    chore: Remove commmented out wip code from use_fetch

--- client/src/hooks/use_fetch/use_fetch.tsx
+++ client/src/hooks/use_fetch/use_fetch.tsx
@@ -1,46 +1,19 @@
 import { useState, useEffect } from "react";
 import { isError } from "../../helpers/is_error";
 
-/*const RESTFUL_METHODS = ["GET", "POST"] as const;
-type RestfulMethod = (typeof RESTFUL_METHODS)[number];*/
-
 export interface FetchReturn<TData> {
   data: TData | undefined;
   isFetching: boolean;
   errorMessage: string | undefined;
 }
 
-export function useFetch<TData>(
-  url: string
-  //method: RestfulMethod = "GET",
-  //body: object = {}
-): FetchReturn<TData> {
+export function useFetch<TData>(url: string): FetchReturn<TData> {
   const [data, setData] = useState<TData>();
   const [isFetching, setIsFetching] = useState(true);
   const [errorMessage, setErrorMessage] = useState<string>();
 
   useEffect(() => {
     const abortController = new AbortController();
-
-    /*let requestOptions: RequestInit | undefined;
-
-    switch (method) {
-      case "POST":
-        requestOptions = {
-          method: method,
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(body),
-          signal: abortController.signal,
-        };
-        break;
-      case "GET":
-        requestOptions = { signal: abortController.signal };
-        break;
-      default:
-        requestOptions = { signal: abortController.signal };
-    }*/
 
     const fetchData = async () => {
       try {
@@ -65,14 +38,16 @@ export function useFetch<TData>(
         }
       } catch (e: unknown) {
         setIsFetching(false);
-        console.log(
-          isError(e) && e.name !== "AbortError" ? e.message : "Unknown error!"
-        );
+        console.log(isError(e) ? e.message : "Unknown error!");
       }
     };
     fetchData();
     return () => abortController.abort();
-  }, [url /*, method, body*/]);
+  }, [url]);
 
-  return { isFetching: isFetching, data: data, errorMessage: errorMessage };
+  return {
+    isFetching: isFetching,
+    data: data,
+    errorMessage: errorMessage,
+  };
 }
